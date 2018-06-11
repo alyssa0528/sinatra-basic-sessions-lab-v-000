90267daa2aaaac2191ab466ee828bdc5967d5290
Alyssa Kim <alyssa.kim@gmail.com>
2018-06-11 12:54:40 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -15,8 +15,7 @@ class App < Sinatra::Base
   post '/checkout' do
     session["item"] = params["item"]
     @session = session #session is the session hash
-
-    "<%= @session["item"] %>"
+    binding.pry
   end
 
 end
